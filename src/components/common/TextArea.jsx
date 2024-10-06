1a93e148881b31a2697592037a2f4eb169594521
achmadakif <achmadakif7@gmail.com>
2024-10-06 23:58:31 +0700

    refactor(chore): adjust indentation

--- src/components/common/TextArea.jsx
+++ src/components/common/TextArea.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 
 function TextArea({ handleNotesContentInput, notesContentInput }) {
   return (
-    <textarea onChange={handleNotesContentInput} value={notesContentInput} placeholder='Tuliskan catatanmu disini...' className='bg-[#18181B] border-2 rounded-md px-4 py-3 h-48'></textarea>
+    <textarea
+      onChange={handleNotesContentInput}
+      value={notesContentInput}
+      placeholder='Tuliskan catatanmu disini...'
+      className='bg-[#18181B] border-2 rounded-md px-4 py-3 h-48'
+    ></textarea>
   );
 }
 
