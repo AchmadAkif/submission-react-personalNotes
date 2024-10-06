71e091d7e27e74f6dc3c7477a2fd2bc316d4609b
achmadakif <achmadakif7@gmail.com>
2024-10-06 23:59:03 +0700

    refactor(chore): adjust indentation

--- src/components/common/TitleInput.jsx
+++ src/components/common/TitleInput.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 
 function TitleInput({ handleNotesTitleInput, notesTitleInput }) {
   return (
-    <input onChange={handleNotesTitleInput} maxLength={50} value={notesTitleInput} type="text" placeholder='Masukkan judul...' className='bg-[#18181B] border-2 rounded-md px-4 py-2' />
+    <input
+      onChange={handleNotesTitleInput}
+      value={notesTitleInput}
+      type="text"
+      placeholder='Masukkan judul...'
+      className='bg-[#18181B] border-2 rounded-md px-4 py-2'
+    />
   );
 }
 
