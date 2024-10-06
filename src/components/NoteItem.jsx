036bb9d1fac79dffbcbcce84d31641921856de1e
achmadakif <achmadakif7@gmail.com>
2024-10-06 14:08:54 +0700

    refactor: props drilling

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from './common/Button';
 
-function NoteItem({ noteTitle, noteContent, noteDate }) {
+function NoteItem({ noteId, noteTitle, noteContent, noteDate, handleNoteItemRemove, handleNoteItemArchived }) {
   return (
     <div className='flex flex-col justify-between border-2'>
       <section className='flex flex-col justify-between gap-y-3 p-4'>
@@ -10,8 +10,8 @@ function NoteItem({ noteTitle, noteContent, noteDate }) {
         <p>{noteContent}</p>
       </section>
       <section className='flex justify-between border-t-2'>
-        <Button name='Hapus' style='border-r-2 p-2 w-1/2 font-medium text-[#CE191F]' />
-        <Button name='Arsipkan' style='w-1/2 font-medium text-[#F8FA93]' />
+        <Button name='Hapus' style='border-r-2 p-2 w-1/2 font-medium text-[#CE191F]' noteId={noteId} handleAction={handleNoteItemRemove} />
+        <Button name='Arsipkan' style='w-1/2 font-medium text-[#F8FA93]' noteId={noteId} handleAction={handleNoteItemArchived} />
       </section>
     </div>
   );
